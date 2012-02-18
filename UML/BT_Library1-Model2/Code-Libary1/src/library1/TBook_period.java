e04c7a745c7d0443e3ee6f60aa87eee524e88e6c
Weronika <weronika.stoczek@gmail.com>
2012-02-18 22:40:20 +0100

    komentarz

--- UML/BT_Library1-Model2/Code-Libary1/src/library1/TBook_period.java
+++ UML/BT_Library1-Model2/Code-Libary1/src/library1/TBook_period.java
@@ -12,23 +12,29 @@ public class TBook_period extends TBook implements Serializable {
 
     private Date period;
 
+    @Override
     public Date getPeriod () {
-        return null;
+        return period;
     }
 
+    @Override
     public void setPeriod (Date date) {
+        period=date;
     }
 
+    @Override
     public boolean period_pass (Object data) {
-        return true;
+        Date d=TFactory.mdays((String)data);
+        if(d.after(period))
+            return true;
+        return false;
     }
 
+    @Override
     public void startPeriod (Object data) {
+        setPeriod( TFactory.mdays((String)data));
     }
 
-    public String toString () {
-        return null;
-    }
 
 }
 
